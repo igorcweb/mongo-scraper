2d9019dfa273be213788c166caf544a42f690246
Igor Chernyshev <igorchern07@gmail.com>
2018-10-04 14:32:59 -0500

    connected to mongodb with mlab

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -5,6 +7,7 @@ const exphbs = require('express-handlebars');
 const logger = require('morgan');
 const controllers = require('./controllers');
 const saved = require('./controllers/saved.js');
+const mongoose = require('mongoose');
 
 app.use(express.static('public'));
 
@@ -22,6 +25,16 @@ app.engine(
 );
 
 app.set('view engine', 'handlebars');
+
+mongoose.Promise = Promise;
+
+mongoose
+  .connect(
+    process.env.DB_HOST,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
 
 app.use(controllers);
 app.use('/saved', saved);
